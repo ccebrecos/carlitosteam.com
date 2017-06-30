779748f65cd0d408fbf8ebd1c82ab2bebf3ef633
Carlos GC <c.cebrecos@hotmail.com>
2017-06-30 18:02:56 +0200

    Footer modified

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,11 +1,14 @@
 <hr>
 <ol class="breadcrumb text-right">
-    <li><a href="https://www.instagram.com/carlitos.team/"><img src="{{URL::asset('icon/instagram.png')}}" with="16" height="16" alt="instagram"></a></li>
-    <li><a href="https://goo.gl/0LsFAe"><img src="{{URL::asset('icon/flickr.png')}}" with="16" height="16" alt="flickr"></a></li>
+    <li><a href="https://www.instagram.com/carlitos.team/" target="_blank"> <img src="{{URL::asset('icon/instagram.png')}}" with="16" height="16" alt="instagram"></a></li>
+    <li><a href="https://goo.gl/0LsFAe" target="_blank"><img src="{{URL::asset('icon/flickr.png')}}" with="16" height="16" alt="flickr"></a></li>
 </ol>
 <!-- Footer -->
 <footer>
     <div class="row">
+        <div class"col-lg-12 text-center">
+            <p> &lt;> with ♥ by <a href="https://twitter.com/ccebrecos" target="_blank" /></p>
+        </div>
         <div class="col-lg-12">
             <p>Copyright &copy; Carlitos'team 2017</p>
         </div>
